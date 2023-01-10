f6ae83129cc94578f46c496de3230deb6cd7a763
aaronbarra <aaron.barra@tsoftglobal.com>
2023-01-10 02:59:47 -0300

    Aplicacion Web Service Java

---
+++ src/main/java/com/example/webservicetest/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.example.webservicetest;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hola Aaron, si lees este mensaje, significa que ha salido todo bien";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hola! Te invito a presionar el botón" %>
+</h1>
+<br/>
+<a href="hello-servlet">Presiona Aqui</a>
+</body>
+</html>
