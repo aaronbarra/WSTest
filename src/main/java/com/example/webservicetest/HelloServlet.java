7ce8e0959ee2c3f4dff5c6b4617e3c037fd3878e
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-01-12 14:15:41 -0500

    Update HelloServlet.java

--- src/main/java/com/example/webservicetest/HelloServlet.java
+++ src/main/java/com/example/webservicetest/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hola Aaron, si lees este mensaje, significa que ha salido todo bien";
+        message = "Hola Aaron, tomcat";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
